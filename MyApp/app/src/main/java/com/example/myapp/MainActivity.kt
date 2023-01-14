ed9eeaffe7e167636b883754bb452c781728ce8d
Leona <102565567+alimhanhan@users.noreply.github.com>
2023-01-14 12:07:22 +0900

    Add files via upload

---
+++ MyApp/app/src/main/java/com/example/myapp/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.example.myapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlin.system.measureTimeMillis
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
+
+
