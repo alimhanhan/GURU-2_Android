9cfbdf605d81f5f54a18be0f487279344ab34127
Leona <102565567+alimhanhan@users.noreply.github.com>
2023-04-02 10:28:41 +0900

    Add files via upload

---
+++ guru_frame_layout_project/app/src/main/java/com/example/frame_layout_project/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.frame_layout_project
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
