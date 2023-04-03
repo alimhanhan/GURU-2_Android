3fd5bc0755ec08158250f4242ff0add2c1ea6947
Leona <102565567+alimhanhan@users.noreply.github.com>
2023-04-03 21:07:28 +0900

    Add files via upload

---
+++ GURU2/app/src/main/java/com/example/startapplication/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.example.startapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        var button1 : Button
+        button1 = findViewById<Button>(R.id.button1)
+    }
+}
